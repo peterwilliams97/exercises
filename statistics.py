2af9cdeb7410517ad0330b50d6426597ae8d4f3e
peterwilliams97 <peter.williams.97@gmail.com>
2010-06-12 10:07:49 +1000

    added Fisher exact test

--- statistics.py
+++ statistics.py
@@ -5,7 +5,7 @@ def my_round(x, d):
     return str(decimal.Decimal(str(d*round(x/d)))) # d*round(x/d)
     
 def show_mat(name, mat):
-    print "------------", name, "------------"
+    print '------------', name, '------------'
     m = [[my_round(x, 0.1) for x in line] for line in mat]
     for line in m:
         print line
@@ -53,7 +53,7 @@ def cov_mat(mat):
     return prod
     
 def corr_mat(cov, var):
-    "Calculate corrletion maxtrix from covariance matrix and vector of stddev's"
+    "Calculate correlation maxtrix from covariance matrix and vector of stddev's"
     w = len(var)
     corr = copy.deepcopy(cov)
     for col in range(w):
@@ -62,7 +62,7 @@ def corr_mat(cov, var):
     return corr    
                 
 def parse_line(line):
-    'Read a comma separated line into an array of floats'
+    "Read a comma separated line into an array of floats"
     return map(float, line.strip().split(','))
     #return [float(n) for n in line.strip().split(',')]
     
@@ -86,23 +86,47 @@ def write_csv(filename, matrix):
     '''
     file(filename, 'w').write('\n'.join(map(lambda row: ','.join(map(str,row)), matrix)) + '\n')
     
+def binomialCoefficient(a, b):
+    "Return a!/b!"
+    print 'bc', a, b
+    assert(a >= b)
+    x = 1.0
+    for i in range(b+1, a+1):
+        x = x * float(i)
+    return x
+
+def fisherExact(table):
+    """ Fisher's exact test for a 2x2 contingency table
+        a  b
+        c  d 
+    """
+    a,b = table[0]
+    c,d = table[1]
+    return binomialCoefficient(a+b,a)*binomialCoefficient(c+d,c)/binomialCoefficient(a+b+c+d,a+c)
+        
+    
 if __name__ == '__main__':
-    if len(sys.argv) != 2:  
-        sys.exit("Specicify a csv file")
-    print "argv[1] = ", sys.argv[1]
-    mat = read_csv2(sys.argv[1])
-    show_mat("input", mat)  # print "mat = ", mat
-    mean = get_mean(mat)
-    show_mat("mean", [mean]) #[[line] for line in mean]) #  print "mean = ", mean
-    centered = sub_mat(mat, mean)
-    show_mat("centered", centered) # print "centered = ", centered
-    var = get_var(centered)
-    show_mat("var", [var])
-    cov = cov_mat(centered)
-    show_mat("cov", cov)#   print "cov = ", cov
-    corr = corr_mat(cov, var)
-    show_mat("corr", corr)
-  # prod = mul_mat(mat)
+    if False:
+        if len(sys.argv) != 2:  
+            sys.exit("Specicify a csv file")
+        print "argv[1] = ", sys.argv[1]
+        mat = read_csv2(sys.argv[1])
+        show_mat("input", mat)  # print "mat = ", mat
+        mean = get_mean(mat)
+        show_mat("mean", [mean]) #[[line] for line in mean]) #  print "mean = ", mean
+        centered = sub_mat(mat, mean)
+        show_mat("centered", centered) # print "centered = ", centered
+        var = get_var(centered)
+        show_mat("var", [var])
+        cov = cov_mat(centered)
+        show_mat("cov", cov)#   print "cov = ", cov
+        corr = corr_mat(cov, var)
+        show_mat("corr", corr)
+    table = ((1, 9), (11, 3))
+    print 'table', table
+    f = fisherExact(table)
+    print 'fisherExact', f
+ # prod = mul_mat(mat)
   #  print "prod = ", prod
   #  print "mat = ", mat
 
